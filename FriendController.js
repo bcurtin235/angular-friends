7453288fd4a4916a07cd3e4ae07ada7af56d9285
Brandon Curtin <curtin.brandon@gmail.com>
2014-04-16 20:26:14 -0600

    I can see the Finish Line!
    almost done just fixing bugs

--- FriendController.js
+++ FriendController.js
@@ -1,12 +1,30 @@
-angular.module('angular-friends').controller('friendController', function($scope){
+angular.module('angular-friends').controller('friendController', function($scope,$http){
 
     $scope.test = 'hello world'
-    $.getJSON('https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json',function(myJsonDataFromServer){
-//    $scope.friends=myJsonDataFromServer.results;
-      $scope.$apply(function(){
-         $scope.friends= myJsonDataFromServer.results;
-      });
-    })
+
+
+
+
+
+        $http.get('https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json').success(myJsonDataFromServer);
+       function myJsonDataFromServer(friendsData){
+           $scope.friends=friendsData.results;
+       }
+
+    $scope.search = "";
+    $scope.sortBy='friend_count';
+    $scope.sortOrder='false';
+
+
+
+
+
+//    $.getJSON('https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json',function(myJsonDataFromServer){
+//       $scope.friends= myJsonDataFromServer.results;
+//    $scope.$apply(function(){
+//         $scope.friends= myJsonDataFromServer.results;
+//      });
+
 })
 
 
